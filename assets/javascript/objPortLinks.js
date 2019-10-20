cde39cea78596cb851401f61d01df67e15cf1f8c
martucazpo <martucazpo@gmail.com>
2019-10-20 10:56:07 -0700

    adding git hub button, removing placeholders

--- assets/javascript/objPortLinks.js
+++ assets/javascript/objPortLinks.js
@@ -1,15 +1,18 @@
-function Port(id, name, link, image, description) {
+function Port(id, name, link, image, description,gitHubLink) {
     this.id = id;
     this.name = name;
     this.link = link;
     this.image = image;
     this.description = description;
+    this.gitHubLink = gitHubLink;
     this.card = function () {
         var cardImage = $("<img>");
         var pictImage = this.image;
         cardImage.attr("style", "height:150px;width:auto");
         cardImage.attr("src", pictImage);
         cardImage.attr("class", "card-image-top");
+        cardImage.attr("id", this.id);
+        cardImage.attr("type","button");
         var cardBody = $("<div>");
         cardBody.attr("class", "card-body");
         cardTitle = $("<h5>");
@@ -20,15 +23,19 @@ function Port(id, name, link, image, description) {
         cardText.attr("class", "card-text");
         cardText.text(this.description);
         cardBody.append(cardText);
+        var cardButton = $("<a>");
+        cardButton.attr("role","button");
+        cardButton.attr("class","btn btn-outline-secondary");
+        cardButton.attr("href", this.gitHubLink);
+        cardButton.text("View on GitHub");
+        cardBody.append(cardButton);
         var card = $("<div>");
-        card.attr("id", this.id);
         card.attr("class", "card border-secondary mb-3 card-body text-secondary m-3 p-1");
-        card.attr("type", "button");
         card.append(cardImage);
         card.append(cardBody);
         $("#cardDeck").prepend(card);
     };
-    this.button = function () {
+    this.imageButton = function () {
         var pageLink = this.link;
         var pageId = this.id;
         $(document).on("click", "#" + pageId, function () {
@@ -42,7 +49,8 @@ var trainSched = new Port(
     "Train Schedualer",
     "https://martucazpo.github.io/trainSchedualor/",
     "assets/images/Screenshot (5).png",
-    "An imaginary train schedualer, using firebase"
+    "An imaginary train schedualer, using firebase",
+    "https://github.com/martucazpo/trainSchedualor"
 );
 
 var psychicGame = new Port(
@@ -50,7 +58,8 @@ var psychicGame = new Port(
     "Psychic Game",
     "https://martucazpo.github.io/psychicGame/",
     "assets/images/scott-rodgerson-375874-unsplash.jpg",
-    "A simple letter guessing game, javascript"
+    "A simple letter guessing game, javascript",
+    "https://github.com/martucazpo/psychicGame"
 );
 
 
@@ -59,7 +68,8 @@ var crystalCol = new Port(
     "Crystal Collector",
     "https://martucazpo.github.io/unit-4-game",
     "assets/images/1-CrystalCollector.jpg",
-    "A game played by adding randomly generated numbers to equal a randomly generated total, javascript"
+    "A game played by adding randomly generated numbers to equal a randomly generated total, javascript",
+    "https://github.com/martucazpo/unit-4-game"
 );
 
 var getGiph = new Port(
@@ -67,7 +77,8 @@ var getGiph = new Port(
     "Get Giphy With It",
     "https://martucazpo.github.io/getGiphyWiphIt",
     "assets/images/Screenshot (4).png",
-    "A type of search site for animal gifs, using giphy API, AJAX and jquery"
+    "A type of search site for animal gifs, using giphy API, AJAX and jquery",
+    "https://github.com/martucazpo/getGiphyWiphIt"
 );
 
 var videoSearch = new Port(
@@ -75,7 +86,8 @@ var videoSearch = new Port(
     "Group one Project: Video Search",
     "https://cmoconnor0823.github.io/Group-Project-One/",
     "assets/images/Screenshot (6).png",
-    "A video game search, using two APIs. AJAX, SASS, and JQuery"
+    "A video game search, using two APIs. AJAX, SASS, and JQuery",
+    "https://github.com/Cmoconnor0823/Games-For-All"
 );
 
 var gitHub = new Port(
@@ -83,7 +95,9 @@ var gitHub = new Port(
     "Git Hub Profile and Repositories",
     "https://github.com/martucazpo",
     "assets/images/Screenshot (7).png",
-    "A link to my Git Hub pages"
+    "A link to my Git Hub pages",
+    "https://github.com/martucazpo"
+    
 );
 
 var liriBot = new Port(
@@ -91,7 +105,8 @@ var liriBot = new Port(
     "Liri-Bot",
     "https://martucazpo.github.io/Liri-node-app/",
     "assets/images/Screenshot (12).png",
-    "A node.js application"
+    "A node.js application",
+    "https://github.com/martucazpo/Liri-node-app"
 );
 
 var friendlyFriend = new Port(
@@ -99,7 +114,8 @@ var friendlyFriend = new Port(
     "Friendly-Friends!",
     "https://fierce-chamber-13363.herokuapp.com/",
     "assets/images/Screenshot (1).png",
-    "A full stack friend finder"
+    "A full stack friend finder",
+    "https://github.com/martucazpo/Friendly-Friends-"
 );
 
 var burgers = new Port(
@@ -107,7 +123,8 @@ var burgers = new Port(
     "Burgers!",
     "https://salty-cove-20056.herokuapp.com/",
     "assets/images/Screenshot (2).png",
-    "A full stack burger app using handlebars, OMV, ORM and mysql"
+    "A full stack burger app using handlebars, OMV, ORM and mysql",
+    "https://github.com/martucazpo/Burgers"
 );
 
 var sequelizedBurger = new Port(
@@ -115,7 +132,8 @@ var sequelizedBurger = new Port(
     "Sequelize My Burger Please",
     "https://quiet-crag-21817.herokuapp.com/",
     "assets/images/sequelizedBurger.png",
-    "The Burgers! app but with sequelize"
+    "The Burgers! app but with sequelize",
+    "https://github.com/martucazpo/sequelizeMyBurgerPlease"
 );
 
 var groupProjectTwo = new Port(
@@ -123,7 +141,8 @@ var groupProjectTwo = new Port(
     "Opinionated",
     "https://mlmm-project2.herokuapp.com",
     "assets/images/opinionated.png",
-    "A review app for movies/books/video games. Express, Sequel, JQuery, MySQL, Handlebars"
+    "A review app for movies/books/video games. Express, Sequel, JQuery, MySQL, Handlebars",
+    "https://github.com/mwright8929/Project-2"
 );
 
 var newsOfTheWired = new Port(
@@ -131,7 +150,8 @@ var newsOfTheWired = new Port(
     "News of the Wired",
     "https://agile-mesa-63380.herokuapp.com/articles",
     "assets/images/wired.png",
-    "A Node.js Web Scraper using Mongoose, Mongodb and Handlebars-Express"
+    "A Node.js Web Scraper using Mongoose, Mongodb and Handlebars-Express",
+    "https://github.com/martucazpo/newsOfTheWired"
 );
 
 var clickyGame = new Port(
@@ -139,7 +159,8 @@ var clickyGame = new Port(
     "Clicky Game",
     "https://fierce-ocean-38330.herokuapp.com",
     "assets/images/clicky.png",
-    "A react.js game"
+    "A react.js game",
+    "https://github.com/martucazpo/ClickyGame"
 );
 
 var googleSearchReact = new Port(
@@ -147,36 +168,37 @@ var googleSearchReact = new Port(
     "Google Search React",
     "https://shrouded-inlet-36558.herokuapp.com",
     "assets/images/google.png",
-    "A react.js REST app"
+    "A react.js REST app",
+    "https://github.com/martucazpo/GoogleBooksReact"
 );
 
 $(document).ready(function () {
     trainSched.card();
-    trainSched.button();
+    trainSched.imageButton();
     psychicGame.card();
-    psychicGame.button();
+    psychicGame.imageButton();
     crystalCol.card();
-    crystalCol.button();
+    crystalCol.imageButton();
     getGiph.card();
-    getGiph.button();
+    getGiph.imageButton();
     videoSearch.card();
-    videoSearch.button();
+    videoSearch.imageButton();
     gitHub.card();
-    gitHub.button();
+    gitHub.imageButton();
     liriBot.card();
-    liriBot.button();
+    liriBot.imageButton();
     friendlyFriend.card();
-    friendlyFriend.button();
+    friendlyFriend.imageButton();
     burgers.card();
-    burgers.button();
+    burgers.imageButton();
     sequelizedBurger.card();
-    sequelizedBurger.button();
+    sequelizedBurger.imageButton();
     groupProjectTwo.card();
-    groupProjectTwo.button();
+    groupProjectTwo.imageButton();
     newsOfTheWired.card();
-    newsOfTheWired.button();
+    newsOfTheWired.imageButton();
     clickyGame.card();
-    clickyGame.button();
+    clickyGame.imageButton();
     googleSearchReact.card();
-    googleSearchReact.button();
+    googleSearchReact.imageButton();
 });
