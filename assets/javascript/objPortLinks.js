eff11e9807f908b13c5ed6bd012348154784778b
martucazpo <martucazpo@gmail.com>
2019-11-23 15:48:11 -0700

    spelling

--- assets/javascript/objPortLinks.js
+++ assets/javascript/objPortLinks.js
@@ -191,7 +191,7 @@ var nodeLogin = new Port(
     "Simple Node Login",
     "https://vast-stream-09563.herokuapp.com/login",
     "assets/images/nodeLogin.png",
-    "A simple login without a database from the site 'Web Dev Simplifies'(link in github readme) that uses passport and ejs. Learning more about logins",
+    "A simple login without a database from the site: Web Dev Simplified (link in github readme) that uses passport and ejs. Learning more about logins",
     "https://martucazpo.github.io/NodeLogin"
 );
 
